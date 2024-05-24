922f7b9c4842a53d60b535d1ac3f85598a7e4892
SkyDogzz <thomas.stephan@live.fr>
2024-05-24 03:16:56 +0200

    Add DashboardPage component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return (
+        <div className="text-black dark:text-white max-w-7xl mx-auto mb-20">
+            <h1 className="text-3xl font-semibold text-gray-800 dark:text-white">Tableau de bord</h1>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client"
+
 import Demo from "@/components/Demo"
 import Fonctionnality from "@/components/Fonctionnality"
 import Hero from "@/components/Hero"
 import Footer from "@/components/Footer"
 import Pricing from "@/components/Pricing"
 
+import { useSession } from "next-auth/react"
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+
 export default function HomePage() {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push('/dashboard')
+    }
+  }, [status, router])
 
   return (
     <>
